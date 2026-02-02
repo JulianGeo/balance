9778bd4c5e0c3f8ce148d99b43581a49ead24fc2
Julian Rojas <julianrojasgeo@gmail.com>
2026-02-02 10:23:32 -0500

    Adds dockerfile and env config

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Imagen optimizada para geociencias (trae GDAL, PROJ, GEOS instalados)
+FROM pangeo/pangeo-notebook:latest
+
+# Directorio de trabajo en el contenedor
+WORKDIR /app
+
+# Copiamos los archivos de requerimientos primero para aprovechar la cache
+COPY requirements.txt .
+
+# Instalamos las librerías de Python
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copiamos el resto del código y el archivo config.py
+COPY . .
+
+# Variable para que Python no guarde buffer y veas los prints en tiempo real
+ENV PYTHONUNBUFFERED=1
+
+# Ajuste Crítico: Agregar la subcarpeta al path de Python
+ENV PYTHONPATH="${PYTHONPATH}:/app/modelos_hidroclimaticos"
+
+# Comando para ejecutar el script
+CMD ["python", "modelos_hidroclimaticos/temperature_CHELSA_30.py"]
--- modelos_hidroclimaticos/config.py
+++ modelos_hidroclimaticos/config.py
@@ -1,5 +1,28 @@
-ruta_shape = r"C:\Gis\GEOTERMIA\QGIS_TEAM\SIG_GEOTERMIA_22_05_2025\Chiles Cerro Negro\AOI\AOI_CNN_WGS84\AOI_CNN_WGS84.shp"
-carpeta_salida_precip = r"C:\Code\TIP\Balance_hidrico\results\modelos_hidroclimaticos\database\precipitacion"
-carpeta_salida_temper = (
-    r"C:\Code\TIP\Balance_hidrico\results\modelos_hidroclimaticos\database\temperatura"
-)
+import os
+from pathlib import Path
+
+# 1. Detectar si estamos en Docker o Local
+# Si la variable de entorno no existe, devolvemos 'LOCAL'
+ENTORNO = os.getenv("APP_ENV", "LOCAL")
+
+# 2. Definir la raíz del proyecto dinámicamente
+# Path(__file__).parent.parent obtiene la carpeta raíz desde la subcarpeta 'scripts'
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+if ENTORNO == "TEST":
+    # Rutas dentro del contenedor (Linux style)
+    ruta_shape = "/app/data/shapefiles/area_estudio.shp"
+    carpeta_salida_temper = "/app/results/modelos_hidroclimaticos/database/temperatura"
+
+else:
+    # Rutas en tu PC (Windows style)
+    # Usamos .resolve() para que Python gestione las letras de unidad (C:, D:)
+    ruta_shape = BASE_DIR / "input" / "shapefiles" / "" / "area_estudio.shp"
+    carpeta_salida_temper = (
+        BASE_DIR / "results" / "modelos_hidroclimaticos" / "database" / "temperatura"
+    )
+
+# Convertir a string para evitar errores con librerías viejas
+ruta_shape = str(ruta_shape)
+carpeta_salida_temper = str(carpeta_salida_temper)
